f2d23cda4d7db5d307a4de2ac83eea4ddee8626c
rajithst <rajiththennakoonshady@gmail.com>
2017-02-20 13:02:33 +0530

    a'

--- application/config/database.php
+++ application/config/database.php
@@ -81,12 +81,12 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'rajith',*/
 
-	/*'hostname' => 'eu-cdbr-azure-west-a.cloudapp.net',
+	'hostname' => 'eu-cdbr-azure-west-a.cloudapp.net',
 	 'username' => 'b15558e9c0342b',
-	 'password' => '3343d3cc',*/
-	'hostname' => 'localhost',
+	 'password' => '3343d3cc',
+	/*'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => '',*/
 
 
 	'database' => 'charity',
